7e41ef13bfe2837356f880c664f6a32e248c192b
Zohar-Monsonego <146588286+Zohar-Monsonego@users.noreply.github.com>
2024-03-29 13:35:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from Newton_Rapson import calc_newton_raphson
+from Secant import calc_secant
+'''
+Team members: 
+(1) Zohar Monsonego
+(2) Hodaya Shirazie
+(3) Shulamit Mor Yossef
+'''
+
+if __name__ == '__main__':
+    calc_newton_raphson()
+    calc_secant()
